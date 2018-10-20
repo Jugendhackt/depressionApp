0309866f344570c578985a8177e35a8e53d09654
Bosimax <bosak.m@web.de>
2018-10-20 18:09:49 +0200

    lifeexpectancy 01

--- app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
+++ app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
@@ -30,21 +30,219 @@ public class MainActivity extends AppCompatActivity {
         int timeremaining = 1000000000-secondslived;
     }
 
-   /* public int lifeexpectancy(String ort, String sex){
-        switch (ort){
-            case: break;
-
-
+    public int lifeexpectancy(String ort, String sex) {
+        int expectancy = 0;
+        switch (ort) {
+            case "Baden-Württemberg":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 80;
+                        break;
+                    case "Female":
+                        expectancy = 84;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Bayern":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 79;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Berlin":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 78;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Brandenburg":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 77;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Bremen":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 77;
+                        break;
+                    case "Female":
+                        expectancy = 82;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Hamburg":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 79;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Hessen":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 79;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Mecklenburg-Vorpommern":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 77;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Niedersachsen":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 78;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Nordrhein-Westfahlen":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 78;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Rheinland-Pfalz":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 79;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Saarland":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 78;
+                        break;
+                    case "Female":
+                        expectancy = 82;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Sachsen":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 78;
+                        break;
+                    case "Female":
+                        expectancy = 84;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Sachsen_Anhalt":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 76;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Schleswig-Holstein":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 78;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
+            case "Thüringen":
+                switch (sex) {
+                    case "Male":
+                        expectancy = 77;
+                        break;
+                    case "Female":
+                        expectancy = 83;
+                        break;
+                    default:
+                        expectancy = 81;
+                        break;
+                }
+                break;
         }
-    }ay.substring(0, 2));
-        int month = Integer.parseInt(birthday.substring(3, 5));
-        int year = Integer.parseInt(birthday.substring(7, 11));
-        Calendar cal = Calendar.getInstance();
-        int dayslived = (int) (cal.get(Calendar.YE
-    public void percent(String birthday) {
-        int day = Integer.parseInt(birthdAR) * 365 + cal.get(Calendar.MONTH) * 30.5 + cal.get(Calendar.DAY_OF_MONTH));
-        double percentage = dayslived/
+        return expectancy;
     }
-    */
 
 }
