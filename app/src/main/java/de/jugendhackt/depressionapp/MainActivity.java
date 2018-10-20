217650284a3aba55d530439facd9f6a1a826a5be
Bosimax <bosak.m@web.de>
2018-10-20 17:51:49 +0200

    prozent 3

--- app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
+++ app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
@@ -20,15 +20,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void countdown(int dd, int mm, int yyyy){
+    public void countdown(String birthday){
         int day = Integer.parseInt(birthday.substring(0,2));
         int month = Integer.parseInt(birthday.substring(3,5));
-        int year = Integer.parseint(birthday.substring(7,11));
+        int year = Integer.parseInt(birthday.substring(7,11));
         Calendar cal = Calendar.getInstance();
         int dayslived = (int)(cal.get(Calendar.YEAR)*365+cal.get(Calendar.MONTH)*30.5+cal.get(Calendar.DAY_OF_MONTH));
         int secondslived = dayslived*86400;
         int timeremaining = 1000000000-secondslived;
     }
 
+   /* public int lifeexpectancy(String ort, String sex){
+        switch (ort){
+            case: break;
+
+
+        }
+    }ay.substring(0, 2));
+        int month = Integer.parseInt(birthday.substring(3, 5));
+        int year = Integer.parseInt(birthday.substring(7, 11));
+        Calendar cal = Calendar.getInstance();
+        int dayslived = (int) (cal.get(Calendar.YE
+    public void percent(String birthday) {
+        int day = Integer.parseInt(birthdAR) * 365 + cal.get(Calendar.MONTH) * 30.5 + cal.get(Calendar.DAY_OF_MONTH));
+        double percentage = dayslived/
+    }
+    */
 
 }
