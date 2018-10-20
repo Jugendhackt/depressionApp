3861635ce86170d98afb757bd83091893474b9f5
Quillan Conn <quillan@quillanconn.com>
2018-10-20 14:41:14 +0200

    Versuch einen Spinner hinzuzufügen

--- app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
+++ app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
@@ -2,6 +2,8 @@ package de.jugendhackt.depressionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"Male", "Female"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
