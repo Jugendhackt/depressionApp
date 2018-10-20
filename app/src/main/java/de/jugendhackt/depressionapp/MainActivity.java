8daf3b6d28521693bfe93e28d593a56833dfafff
Quillan Conn <quillan@quillanconn.com>
2018-10-20 16:50:29 +0200

    endlich funktionierende Spinner und bugfixes

--- app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
+++ app/src/main/java/de/jugendhackt/depressionapp/MainActivity.java
@@ -15,16 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Spinner dropdown = findViewById(R.id.spinner);
-        String[] items = new String[]{"Male", "Female"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
 
-        Spinner dropdown1 = findViewById(R.id.spinner1);
-        String[] items1 = new String[]{"Baden-Württemberg", "Bayern", "Berlin", "Brandenburg", "Bremen","Hamburg",
-                "Hessen","Mecklenburg-Vorpommern", "Niedersachsen", "Nordrhein-Westfalen", "Rheinland-Pfalz",
-                "Saarland","Sachsen", "Sachsen-Anhalt", "Schleswig-Holstein", "Thüringen"};
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
-        dropdown1.setAdapter(adapter1);
     }
 }
