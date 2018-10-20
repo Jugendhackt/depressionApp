c0b055a71766ea78bb47ec9149d5949e0d671f5f
Quillan Conn <quillan@quillanconn.com>
2018-10-20 16:20:17 +0200

    Bugfixes and first successful build

---
+++ app/src/main/java/de/jugendhackt/depressionapp/StartActivity.java
@@ -0,0 +1,13 @@
+package de.jugendhackt.depressionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
